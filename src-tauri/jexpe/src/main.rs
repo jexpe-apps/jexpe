9fc32e1a49e68ea528ddfd336beb63b3262f703b
raccoman <riccardoaccoma@gmail.com>
2022-11-29 15:21:52 +0100

    feat(tauri): open_vault command

--- src-tauri/jexpe/src/main.rs
+++ src-tauri/jexpe/src/main.rs
@@ -1,34 +1,42 @@
-use std::borrow::{Borrow, BorrowMut};
-use std::error::Error;
-use tauri::Manager;
+use std::sync::Mutex;
+use tauri::{AppHandle, State};
 use jexpe_vault::{Group, Host, vault};
+use vault::Repo;
 
 mod errors;
 
 struct JexpeVault {
-    repo: Option<vault::Repo>,
+    repo: Mutex<Option<Repo>>,
 }
 
 #[tauri::command]
-pub fn open_vault(
-    app_handle: tauri::AppHandle,
-    jexpe_vault: tauri::State<JexpeVault>,
+fn open_vault(
+    app_handle: AppHandle,
+    jexpe_vault: State<JexpeVault>,
     master_password: String,
 ) -> Result<String, String> {
-
     let app_data_dir = app_handle.path_resolver().app_data_dir()
-        .unwrap_or_else(Err(errors::APP_DATA_DIR_INVALID));
+        .unwrap();
+    let path = app_data_dir.into_os_string().into_string().unwrap();
 
-
-
-    vault::open()
+    let repo = vault::open(&path, "default", master_password);
+    match repo {
+        Ok(repo) => {
+            *jexpe_vault.repo.lock().unwrap() = Some(repo);
+            Ok("Success!".to_string())
+        }
+        Err(_e) => {
+            Err("Invalid password!".to_string())
+        }
+    }
 }
 
 fn main() {
     jexpe_vault::init();
 
     tauri::Builder::default()
-        .manage(JexpeVault { repo: None })
+        .manage(JexpeVault { repo: Mutex::new(None) })
+        .invoke_handler(tauri::generate_handler![open_vault])
         .run(tauri::generate_context!())
         .expect("error while running jexpe application");
 }
