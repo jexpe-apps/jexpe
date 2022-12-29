8217f58ec45649eda6aafa7843b0a73ef11ae917
raccoman <riccardoaccoma@gmail.com>
2022-12-29 16:13:52 +0100

    checking husky

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,13 +1,18 @@
-import {FC} from "react";
-import {SpinnerGap} from "phosphor-react";
+import { FC } from 'react'
+import { SpinnerGap } from 'phosphor-react'
 
 const Component: FC<{
-    size?: number;
-    className?: string;
-}> = ({size, className}) => (
-    <div className='relative'>
-        <SpinnerGap weight='bold' className={`animate-spin ${className ? className : 'text-primary-900'}`}
-                    size={size ?? 54}/>
+    size?: number
+    className?: string
+}> = ({ size, className }) => (
+    <div className="relative">
+        <SpinnerGap
+            weight="bold"
+            className={`animate-spin ${
+                className ? className : 'text-primary-900'
+            }`}
+            size={size ?? 54}
+        />
     </div>
-);
-export default Component;
+)
+export default Component
