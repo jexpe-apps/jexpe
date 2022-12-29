50da03f21f115fad7d87e70739a82221cdfc3af7
raccoman <riccardoaccoma@gmail.com>
2022-12-29 16:10:13 +0100

    configure husky + lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: 'tsconfig.json',
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/strict',
+        'prettier',
+    ],
+    root: true,
+}
