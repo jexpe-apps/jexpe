1ba4a66f0be22f61f6b4442f6feb8ab7d1a9e7ad
raccoman <riccardoaccoma@gmail.com>
2023-01-09 14:15:22 +0100

    disable SSR

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ import dynamic from 'next/dynamic'
 
 export { default as Spinner } from './spinner'
 export { default as TabManager } from './tab_manager'
-export const SafeHydrate = dynamic(() => import('./safe_hydrate'), { ssr: false })
+export { default as SafeHydrate } from './safe_hydrate'
 export const DynamicTerminal = dynamic(() => import('./xterm'), { ssr: false })
