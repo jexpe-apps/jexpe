062048f3aa0d2f0cf79b0fcdebe7d0872d315293
raccoman <riccardoaccoma@gmail.com>
2023-01-23 22:49:40 +0100

    UI improvements

--- src/components/tab_controller/components/options/index.tsx
+++ src/components/tab_controller/components/options/index.tsx
@@ -7,40 +7,27 @@ import { Center, Flex } from 'src/components'
 
 const Component: FC = () => {
     const { shells, spawnShell } = useTerminal()
-    
-    const [open, setOpen] = useState(false)
 
-    const showModal = () => {
-        setOpen(true)
-    }
-
-    const handleOk = (e: React.MouseEvent<HTMLElement>) => {
-        console.log(e)
-        setOpen(false)
-    }
-
-    const handleCancel = (e: React.MouseEvent<HTMLElement>) => {
-        console.log(e)
-        setOpen(false)
-    }
+    const [isModalOpen, setModalOpen] = useState(false)
 
     return (
         <Space.Compact>
             <Button
+                onClick={() => spawnShell(shells[0])}
                 icon={
                     <Center>
                         <Plus />
                     </Center>
                 }
             />
-            
-            <Modal title="Select profile" open={open} onOk={handleOk} onCancel={handleCancel} footer={null}>
+
+            <Modal title={null} closable={false} open={isModalOpen} footer={null} onCancel={() => setModalOpen(false)}>
                 <Flex direction="column" className="gap-2">
                     {shells.map((shell, index) => (
                         <Button
                             key={index}
                             onClick={() => {
-                                setOpen(false)
+                                setModalOpen(false)
                                 spawnShell(shell)
                             }}
                             className="w-full flex items-center gap-2"
@@ -56,7 +43,7 @@ const Component: FC = () => {
             </Modal>
 
             <Button
-                onClick={showModal}
+                onClick={() => setModalOpen(true)}
                 icon={
                     <Center>
                         <CaretDown />
