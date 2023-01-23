115cafd904bc60e1777f57e023fef0543871ba6d
Riccardo Accomando <riccardoaccoma@gmail.com>
2023-01-23 23:43:45 +0100

    xtermjs addons

--- src/components/xterm/index.tsx
+++ src/components/xterm/index.tsx
@@ -1,49 +1,70 @@
 import { FC, useEffect, useMemo, useRef } from 'react'
 import { FitAddon } from 'xterm-addon-fit'
 import { useSize } from 'ahooks'
+import { Unicode11Addon } from 'xterm-addon-unicode11'
+import { WebglAddon } from 'xterm-addon-webgl'
+// import { LigaturesAddon } from 'xterm-addon-ligatures'
+// import { ImageAddon } from 'xterm-addon-image'
+import { WebLinksAddon } from 'xterm-addon-web-links'
 
 import type { ITerminal } from 'src/contexts/terminal/types'
 
 const Component: FC<{ terminal: ITerminal; focused: boolean }> = ({ terminal, focused }) => {
-    const target = useRef<HTMLDivElement | null>(null)
+	const target = useRef<HTMLDivElement | null>(null)
 
-    const fitAddon = useMemo(() => new FitAddon(), [])
-    const size = useSize(target)
+	const fitAddon = useMemo(() => new FitAddon(), [])
+	const size = useSize(target)
 
-    useEffect(() => {
-        if (!focused) {
-            return
-        }
+	useEffect(() => {
+		if (!focused) {
+			return
+		}
 
-        fitAddon.fit()
-    }, [size, focused])
+		fitAddon.fit()
+	}, [size, focused])
 
-    useEffect(() => {
-        terminal.xterm.focus()
+	useEffect(() => {
+		terminal.xterm.focus()
 
-        return () => terminal.xterm.blur()
-    }, [focused])
+		return () => terminal.xterm.blur()
+	}, [focused])
 
-    useEffect(() => {
-        if (!target.current) {
-            return
-        }
+	useEffect(() => {
+		if (!target.current) {
+			return
+		}
 
-        terminal.xterm.open(target.current)
-        terminal.xterm.focus()
+		terminal.xterm.open(target.current)
+		terminal.xterm.focus()
 
-        // Activate the xtermjs fit-addon
-        terminal.xterm.loadAddon(fitAddon)
-        fitAddon.activate(terminal.xterm)
+		// Activate the xtermjs fit-addon
+		terminal.xterm.loadAddon(fitAddon)
+		fitAddon.activate(terminal.xterm)
 
-        fitAddon.fit()
+		// Activate xtermjs unicode11-addon
+		terminal.xterm.loadAddon(new Unicode11Addon())
+		terminal.xterm.unicode.activeVersion = '11'
 
-        return () => {
-            // TODO: Destroy terminal and kill pty
-        }
-    }, [])
+		// Activate the xtermjs webgl-addon
+		terminal.xterm.loadAddon(new WebglAddon())
 
-    return <div ref={target} className="h-full w-full overflow-hidden" style={{ display: focused ? 'flex' : 'none' }} />
+		// Activate the xtermjs image-addon
+		// terminal.xterm.loadAddon(new ImageAddon())
+
+		// Activate the xtermjs font-ligatures-addon
+		// terminal.xterm.loadAddon(new LigaturesAddon())
+
+		// Activate the xtermjs web-links-addon
+		terminal.xterm.loadAddon(new WebLinksAddon())
+
+		fitAddon.fit()
+
+		return () => {
+			// TODO: Destroy terminal and kill pty
+		}
+	}, [])
+
+	return <div ref={target} className="h-full w-full overflow-hidden" style={{ display: focused ? 'flex' : 'none' }} />
 }
 
 export default Component
--- src/contexts/terminal/index.tsx
+++ src/contexts/terminal/index.tsx
@@ -9,157 +9,159 @@ import type { FCWithChildren } from 'src/types'
 import type { ITerminal, ITerminalContext, IPTYSpawnPayload, IPTYExitPayload, IPTYSdoutPayload, ISystemShell } from './types'
 
 const TerminalContext = createContext<ITerminalContext>({
-    shells: [],
-    spawnShell: () => undefined,
-    terminals: [],
-    focused: undefined,
-    focus: () => undefined,
+	shells: [],
+	spawnShell: () => undefined,
+	terminals: [],
+	focused: undefined,
+	focus: () => undefined,
 })
 
 export const TerminalContextProvider: FCWithChildren = ({ children }) => {
-    const [shells, setShells] = useState<ISystemShell[]>([])
-    const [terminals, setTerminals] = useState<ITerminal[]>([])
-    const [focused, setFocused] = useState<string | undefined>(undefined)
+	const [shells, setShells] = useState<ISystemShell[]>([])
+	const [terminals, setTerminals] = useState<ITerminal[]>([])
+	const [focused, setFocused] = useState<string | undefined>(undefined)
 
-    const router = useRouter()
+	const router = useRouter()
 
-    const spawnShell = (shell: ISystemShell) => {
-        invoke(PTY_SPAWN_COMMAND, { shell }).catch(console.error)
-    }
+	const spawnShell = (shell: ISystemShell) => {
+		invoke(PTY_SPAWN_COMMAND, { shell }).catch(console.error)
+	}
 
-    useEffect(() => {
-        invoke<ISystemShell[]>(GET_SYSTEM_SHELLS_COMMAND, {}).then(setShells).catch(console.error)
-    }, [])
+	useEffect(() => {
+		invoke<ISystemShell[]>(GET_SYSTEM_SHELLS_COMMAND, {}).then(setShells).catch(console.error)
+	}, [])
 
-    useEffect(() => {
-        const spawnListener = listen<IPTYSpawnPayload>(PTY_SPAWN_EVENT, ({ payload }) => {
-            const { id, shell } = payload
+	useEffect(() => {
+		const spawnListener = listen<IPTYSpawnPayload>(PTY_SPAWN_EVENT, ({ payload }) => {
+			const { id, shell } = payload
 
-            // Dynamically import xterm to ensure it's only loaded client-side
-            import('xterm')
-                .then(({ Terminal }) => {
-                    // Create a new xterm instance
-                    const xterm = new Terminal({
-                        // TODO: Add possibility to customize theme
-                        theme: {
-                            background: '#1A1B1E',
-                            cursor: '#10B981',
-                            cursorAccent: '#10B98100',
-                        },
-                        fontFamily: 'Cascadia Mono, MesloLGS NF, Monospace',
-                        fontWeight: 'normal',
-                        fontSize: 14,
-                        cursorBlink: true,
-                    })
+			// Dynamically import xterm to ensure it's only loaded client-side
+			import('xterm')
+				.then(({ Terminal }) => {
+					// Create a new xterm instance
+					const xterm = new Terminal({
+						// TODO: Add possibility to customize theme
+						theme: {
+							background: '#1A1B1E',
+							cursor: '#10B981',
+							cursorAccent: '#10B98100',
+						},
+						fontFamily: 'Cascadia Mono, MesloLGS NF, Monospace',
+						fontWeight: 'normal',
+						fontSize: 14,
+						cursorBlink: true,
+						allowTransparency: true,
+						allowProposedApi: true,
+						overviewRulerWidth: 8,
+					})
 
-                    xterm.onData((data) =>
-                        invoke(PTY_STDIN_COMMAND, { id, data })
-                            // TODO: Handle errors properly
-                            .catch(console.error)
-                    )
+					xterm.onData((data) =>
+						invoke(PTY_STDIN_COMMAND, { id, data })
+							// TODO: Handle errors properly
+							.catch(console.error)
+					)
 
-                    xterm.onResize((size) =>
-                        invoke(PTY_RESIZE_COMMAND, {
-                            id,
-                            size: {
-                                ...size,
-                                // TODO: Retrieve char width and height
-                                pixel_width: 0,
-                                pixel_height: 0,
-                            },
-                        })
-                            // TODO: Handle errors properly
-                            .catch(console.error)
-                    )
+					xterm.onResize((size) =>
+						invoke(PTY_RESIZE_COMMAND, {
+							id,
+							size: {
+								...size,
+								// TODO: Retrieve char width and height
+								pixel_width: 0,
+								pixel_height: 0,
+							},
+						})
+							// TODO: Handle errors properly
+							.catch(console.error)
+					)
 
-                    xterm.onTitleChange((title) => {
-                        setTerminals((terminals) =>
-                            terminals.map((terminal) => {
-                                if (terminal.id === id) {
-                                    return {
-                                        ...terminal,
-                                        title,
-                                    }
-                                }
-                                return terminal
-                            })
-                        )
-                    })
+					xterm.onTitleChange((title) => {
+						setTerminals((terminals) =>
+							terminals.map((terminal) => {
+								if (terminal.id === id) {
+									return {
+										...terminal,
+										title,
+									}
+								}
+								return terminal
+							})
+						)
+					})
 
-                    // Add the terminal to the context
-                    setTerminals((terminals) => [
-                        ...terminals,
-                        {
-                            id,
-                            shell,
-                            title: shell.name,
-                            xterm,
-                        },
-                    ])
+					// Add the terminal to the context
+					setTerminals((terminals) => [
+						...terminals,
+						{
+							id,
+							shell,
+							title: shell.name,
+							xterm,
+						},
+					])
 
-                    // Focus the new terminal
-                    setFocused(id)
+					// Focus the new terminal
+					setFocused(id)
 
-                    if (router.asPath !== '/terminal') {
-                        void router.push('/terminal')
-                    }
-                    
-                })
-                // TODO: Maybe kill pty, just to be on the safe side (?)
-                .catch(console.error)
-        })
+					if (router.asPath !== '/terminal') {
+						void router.push('/terminal')
+					}
+				})
+				// TODO: Maybe kill pty, just to be on the safe side (?)
+				.catch(console.error)
+		})
 
-        const stdoutListener = listen<IPTYSdoutPayload>(PTY_STDOUT_EVENT, ({ payload }) => {
-            const { id, bytes } = payload
+		const stdoutListener = listen<IPTYSdoutPayload>(PTY_STDOUT_EVENT, ({ payload }) => {
+			const { id, bytes } = payload
 
-            // Find the terminal with the given id
-            const terminal = terminals.find((terminal) => terminal.id === id)
-            if (!terminal) {
-                // TODO: Maybe kill pty, just to be on the safe side (?)
-                console.error(`[STDOUT-LISTENER] Could not find terminal with id ${id}`)
-                return
-            }
+			// Find the terminal with the given id
+			const terminal = terminals.find((terminal) => terminal.id === id)
+			if (!terminal) {
+				// TODO: Maybe kill pty, just to be on the safe side (?)
+				console.error(`[STDOUT-LISTENER] Could not find terminal with id ${id}`)
+				return
+			}
 
-            // Write the bytes to the xterm instance
-            terminal.xterm.write(bytes)
-        })
+			// Write the bytes to the xterm instance
+			terminal.xterm.write(bytes)
+		})
 
-        const exitListener = listen<IPTYExitPayload>(PTY_EXIT_EVENT, ({ payload }) => {
-            const { id, success, code } = payload
+		const exitListener = listen<IPTYExitPayload>(PTY_EXIT_EVENT, ({ payload }) => {
+			const { id, success, code } = payload
 
-            // Find the terminal with the given id
-            const terminal = terminals.find((terminal) => terminal.id === id)
-            if (!terminal) {
-                // This should never happen, but just to be on the safe side
-                console.error(`[EXIT-LISTENER] Could not find terminal with id ${id}`)
-                return
-            }
+			// Find the terminal with the given id
+			const terminal = terminals.find((terminal) => terminal.id === id)
+			if (!terminal) {
+				// This should never happen, but just to be on the safe side
+				console.error(`[EXIT-LISTENER] Could not find terminal with id ${id}`)
+				return
+			}
 
-            // TODO: Handle `success` and `code` properly
-            void success
-            void code
+			// TODO: Handle `success` and `code` properly
+			void success
+			void code
 
-            // Remove the terminal from the context
-            setTerminals((terminals) => {
-                terminals = terminals.filter((terminal) => terminal.id !== id)
+			// Remove the terminal from the context
+			setTerminals((terminals) => {
+				terminals = terminals.filter((terminal) => terminal.id !== id)
 
-                // Focus the nearby left/right terminal
-                setFocused(terminals.length > 0 ? terminals[0].id : undefined)
+				// Focus the nearby left/right terminal
+				setFocused(terminals.length > 0 ? terminals[0].id : undefined)
 
-                return terminals
-            })
-        })
+				return terminals
+			})
+		})
 
-        return () => {
-            spawnListener.then((unlisten) => unlisten()).catch(console.error)
+		return () => {
+			spawnListener.then((unlisten) => unlisten()).catch(console.error)
 
-            stdoutListener.then((unlisten) => unlisten()).catch(console.error)
+			stdoutListener.then((unlisten) => unlisten()).catch(console.error)
 
-            exitListener.then((unlisten) => unlisten()).catch(console.error)
-        }
-    }, [terminals])
+			exitListener.then((unlisten) => unlisten()).catch(console.error)
+		}
+	}, [terminals])
 
-    return <TerminalContext.Provider value={{ shells, spawnShell, terminals, focused, focus: setFocused }}>{children}</TerminalContext.Provider>
+	return <TerminalContext.Provider value={{ shells, spawnShell, terminals, focused, focus: setFocused }}>{children}</TerminalContext.Provider>
 }
 
 export const useTerminal = () => useContext(TerminalContext)
