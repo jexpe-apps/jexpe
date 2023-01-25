c547fc767fb49ec1345e7ee8077126afd79353fd
Riccardo Accomando <riccardoaccoma@gmail.com>
2023-01-25 19:05:04 +0100

    feat: closable tab (#37)

--- src/components/tab_controller/components/options/index.tsx
+++ src/components/tab_controller/components/options/index.tsx
@@ -6,55 +6,55 @@ import Image from 'next/image'
 import { Center, Flex } from 'src/components'
 
 const Component: FC = () => {
-    const { shells, spawnShell } = useTerminal()
+	const { shells, spawnPty } = useTerminal()
 
-    const [isModalOpen, setModalOpen] = useState(false)
+	const [isModalOpen, setModalOpen] = useState(false)
 
-    const closeModal = () => setModalOpen(false)
-    const openModal = () => setModalOpen(true)
+	const closeModal = () => setModalOpen(false)
+	const openModal = () => setModalOpen(true)
 
-    return (
-        <Space.Compact>
-            <Button
-                onClick={() => spawnShell(shells[0])}
-                icon={
-                    <Center>
-                        <Plus />
-                    </Center>
-                }
-            />
+	return (
+		<Space.Compact>
+			<Button
+				onClick={() => spawnPty(shells[0])}
+				icon={
+					<Center>
+						<Plus />
+					</Center>
+				}
+			/>
 
-            <Modal title={null} closable={false} open={isModalOpen} footer={null} onCancel={closeModal}>
-                <Flex direction="column" className="gap-2">
-                    {shells.map((shell, index) => (
-                        <Button
-                            key={index}
-                            onClick={() => {
-                                closeModal()
-                                spawnShell(shell)
-                            }}
-                            className="w-full flex items-center gap-2"
-                            type="text"
-                        >
-                            <Image height={20} width={20} src={shell.icon} alt="system-shell" />
-                            <Typography.Text>
-                                {shell.name} <span className="text-gray-600">({shell.command})</span>
-                            </Typography.Text>
-                        </Button>
-                    ))}
-                </Flex>
-            </Modal>
+			<Modal title={null} closable={false} open={isModalOpen} footer={null} onCancel={closeModal}>
+				<Flex direction="column" className="gap-2">
+					{shells.map((shell, index) => (
+						<Button
+							key={index}
+							onClick={() => {
+								closeModal()
+								spawnPty(shell)
+							}}
+							className="w-full flex items-center gap-2"
+							type="text"
+						>
+							<Image height={20} width={20} src={shell.icon} alt="system-shell" />
+							<Typography.Text>
+								{shell.name} <span className="text-gray-600">({shell.command})</span>
+							</Typography.Text>
+						</Button>
+					))}
+				</Flex>
+			</Modal>
 
-            <Button
-                onClick={openModal}
-                icon={
-                    <Center>
-                        <CaretDown />
-                    </Center>
-                }
-            />
-        </Space.Compact>
-    )
+			<Button
+				onClick={openModal}
+				icon={
+					<Center>
+						<CaretDown />
+					</Center>
+				}
+			/>
+		</Space.Compact>
+	)
 }
 
 export default Component
